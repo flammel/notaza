82d7a7abb01bbc6a545e951c199728f8ecd980bc
Florian Lammel <mail@florianlammel.com>
2020-07-08 19:33:26 +0200

    remove console.log

--- client/src/selectors/activePage.ts
+++ client/src/selectors/activePage.ts
@@ -5,6 +5,5 @@ export const selectActivePage = createSelector((state): Page | undefined => {
     if (state.activePage === undefined) {
         return undefined;
     }
-    console.log('select ' + state.activePage);
     return state.pages.find((page) => page.id === state.activePage);
 });
