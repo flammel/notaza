11700d3548fd25eb1716d762398388b80654d369
Florian Lammel <mail@florianlammel.com>
2020-09-26 14:55:18 +0200

    tree nav

--- src/View.ts
+++ src/View.ts
@@ -3,6 +3,57 @@ import { Page } from './Page';
 import { getSearchResults } from './search';
 import { getBacklinks } from './backlinks';
 
+interface PageTree {
+    page: Page;
+    children: PageTree[];
+}
+
+function isChildOf(child: Page, parent: Page): boolean {
+    return parent.filename === child.filename.split('.').slice(0, -2).join('.') + '.md';
+}
+
+function buildPageTree(pages: Page[], query: string): PageTree[] {
+    if (query !== '') {
+        return getSearchResults(pages, query).map((result) => ({
+            page: result.page,
+            children: [],
+        }));
+    }
+
+    const trees: PageTree[] = [];
+    const stack: PageTree[] = [];
+
+    const sorted = pages.sort((a, b) => a.filename.slice(0, -3).localeCompare(b.filename.slice(0, -3)));
+    for (const page of sorted) {
+        let top = stack.pop();
+        const tree = { page, children: [] };
+        if (top !== undefined) {
+            while (top && !isChildOf(page, top.page)) {
+                top = stack.pop();
+            }
+            if (top) {
+                top.children.push(tree);
+                stack.push(top);
+            } else {
+                trees.push(tree);
+            }
+        }
+        stack.push(tree);
+    }
+
+    return trees;
+}
+
+function findPage(pages: Page[], url: string): Page {
+    const found = pages.find((page) => page.filename === url);
+    if (found === undefined) {
+        const title = url.slice(0, -3);
+        return new Page(url, undefined, `---\ntitle:${title}\n---\n`);
+    } else {
+        return found;
+    }
+}
+
 export class View {
     private readonly $content: HTMLElement;
     private readonly $results: HTMLElement;
@@ -27,7 +78,7 @@ export class View {
         this.$content.classList.add('content');
 
         this.$results = document.createElement('ul');
-        this.$results.classList.add('sidebar__list');
+        this.$results.classList.add('sidebar__list', 'sidebar__list--root');
 
         const $app = document.createElement('div');
         $app.classList.add('app');
@@ -57,12 +108,13 @@ export class View {
     }
 
     private markSidebarItemActive(): void {
-        for (const $result of this.$results.children) {
-            const $link = $result.querySelector('a');
-            if ($link?.getAttribute('href') === '/#/' + this.url) {
-                $link.classList.add('active');
-            } else if ($link?.classList.contains('active')) {
-                $link.classList.remove('active');
+        this.$results.querySelector('.active')?.classList.remove('active');
+        const activeItem = this.$results.querySelector('a[href="/#/' + this.url + '"]');
+        if (activeItem) {
+            activeItem.classList.add('active');
+            let parent: Element | null | undefined = activeItem;
+            while ((parent = parent.parentElement?.closest('.sidebar__item'))) {
+                parent.classList.add('sidebar__item--open');
             }
         }
     }
@@ -89,14 +141,44 @@ export class View {
         return $sidebar;
     }
 
-    private findPage(): Page {
-        const found = this.pages.find((page) => page.filename === this.url);
-        if (found === undefined) {
-            const title = this.url.slice(0, -3);
-            return new Page(this.url, undefined, `---\ntitle:${title}\n---\n`);
-        } else {
-            return found;
+    private renderTree(tree: PageTree): HTMLElement {
+        const $item = document.createElement('li');
+        $item.classList.add('sidebar__item');
+
+        const $link = document.createElement('a');
+        $link.classList.add('sidebar__link');
+        $link.setAttribute('href', '/#/' + tree.page.filename);
+        $link.innerHTML = tree.page.title;
+        $item.appendChild($link);
+
+        if (tree.children.length > 0) {
+            const $opener = document.createElement('span');
+            $opener.classList.add('sidebar__item-opener');
+            $opener.innerText = '+';
+            $opener.addEventListener('click', () => {
+                $item.classList.toggle('sidebar__item--open');
+            });
+            $item.appendChild($opener);
+
+            const $children = document.createElement('ul');
+            $children.classList.add('sidebar__list');
+            for (const child of tree.children) {
+                $children.appendChild(this.renderTree(child));
+            }
+            $item.appendChild($children);
         }
+
+        return $item;
+    }
+
+    private renderSearchResults(): void {
+        const $container = document.createDocumentFragment();
+        const trees = buildPageTree(this.pages, this.query);
+        for (const tree of trees) {
+            $container.appendChild(this.renderTree(tree));
+        }
+        this.$results.innerHTML = '';
+        this.$results.appendChild($container);
     }
 
     private renderPage(): void {
@@ -105,7 +187,7 @@ export class View {
             return;
         }
 
-        const page = this.findPage();
+        const page = findPage(this.pages, this.url);
         const $page = document.createElement('div');
         $page.classList.add('page');
         $page.innerHTML = this.markdownRenderer.render(page);
@@ -130,32 +212,6 @@ export class View {
         document.title = 'KB | ' + page.title;
     }
 
-    private renderSearchResults(): void {
-        const $container = document.createDocumentFragment();
-        for (const result of getSearchResults(this.pages, this.query)) {
-            const $result = document.createElement('li');
-            $result.classList.add('sidebar__result');
-
-            const $link = document.createElement('a');
-            $link.setAttribute('href', '/#/' + result.page.filename);
-            $link.innerHTML = result.page.title;
-
-            const $matches = document.createElement('ul');
-            for (const match of result.matches) {
-                const $match = document.createElement('li');
-                $match.classList.add('search-result__match');
-                $match.innerHTML = match;
-                $matches.appendChild($match);
-            }
-
-            $result.appendChild($link);
-            $result.appendChild($matches);
-            $container.appendChild($result);
-        }
-        this.$results.innerHTML = '';
-        this.$results.appendChild($container);
-    }
-
     private renderBacklinks(page: Page): Node {
         const $fragment = document.createDocumentFragment();
 
@@ -164,17 +220,20 @@ export class View {
         $fragment.appendChild($headline);
 
         for (const pageWithBacklinks of getBacklinks(this.markdownRenderer, this.pages, page)) {
+            const $container = document.createElement('div');
+            $container.classList.add('reference');
             const $title = document.createElement('h3');
             const $link = document.createElement('a');
             $link.setAttribute('href', '/#/' + pageWithBacklinks.page.filename);
             $link.innerText = pageWithBacklinks.page.title;
             $title.appendChild($link);
-            $fragment.appendChild($title);
+            $container.appendChild($title);
             for (const backlink of pageWithBacklinks.backlinks) {
                 const $backlink = document.createElement('div');
                 $backlink.innerHTML = this.markdownRenderer.renderTokens(backlink.content);
-                $fragment.appendChild($backlink);
+                $container.appendChild($backlink);
             }
+            $fragment.appendChild($container);
         }
 
         return $fragment;
