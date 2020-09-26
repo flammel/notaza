d96c07689612615aaf90f46a0dfc5afa51d56a40
Florian Lammel <mail@florianlammel.com>
2020-09-26 12:02:48 +0200

    remove unused util functions

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,3 @@
-function padLeadingZero(x: number): string {
-    return x.toString().padStart(2, '0');
-}
-
 export function notUndefined<T>(x: T | undefined): x is T {
     return x !== undefined;
 }
@@ -9,25 +5,4 @@ export function notUndefined<T>(x: T | undefined): x is T {
 // https://fettblog.eu/typescript-hasownproperty/
 export function hasOwnProperty<X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> {
     return obj.hasOwnProperty(prop);
-}
-
-export function dateToString(date: Date): string {
-    return date.getFullYear() + '-' + padLeadingZero(date.getMonth() + 1) + '-' + padLeadingZero(date.getDate());
-}
-
-/**
- * https://stackoverflow.com/a/1349426
- */
-export function makeId(): string {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < 16; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-
-export function assertNever(x: never): never {
-    throw new Error('Unexpected object: ' + x);
 }
